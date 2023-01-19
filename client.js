ba55287d75cc52b328f5c5121ff48a449a4fb4ba
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-18 18:00:32 -0700

    sending a name

--- client.js
+++ client.js
@@ -12,6 +12,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+    conn.write("Name: XDN");
+  });
+
   conn.on("data", message => {
     // code that does something when the connection is first established
     console.log(message);
