b4db9aebec8e70955cf77f5694c1b507c90c62b6
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-18 18:25:23 -0700

    input module

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  stdin.on("data", (handleUserInput));
-  
-  return stdin;
-};
-
-setupInput();
 console.log("Connecting ...");
 connect();
+setupInput();
