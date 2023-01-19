92f208cfae7d48973b83e6b70aa6b2c9807d51f0
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-18 18:41:06 -0700

    snake movements

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require("net");
 const host = "165.227.47.243";
 const port = "50541"
 // establishes a connection with the game server
-const connect = function() {
+const connect = () => {
   const conn = net.createConnection({
     host, // IP address here,
     port // PORT number here,
--- input.js
+++ input.js
@@ -1,19 +1,40 @@
-const handleUserInput = function(key) {
+let connection;
+
+const handleUserInput = key => {
   // your code here
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
+  }
+
+  switch (key) {
+    case "\u0077":
+      connection.write("Move: up");
+      break;
+    case "\u0061":
+      connection.write("Move: left");
+      break;
+    case "\u0073":
+      connection.write("Move: down");
+      break;
+    case "\u0064":
+      connection.write("Move: right");
+      break;
+    default:
+      break;
   }
 };
 
-const setupInput = function () {
+const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   
-  stdin.on("data", (handleUserInput));
+  stdin.on("data", handleUserInput);
   
   return stdin;
 };
+
 
 module.exports = { setupInput };
